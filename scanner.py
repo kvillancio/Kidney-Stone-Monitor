1828ffeb26aadf7eee5a86ac7f49c1a9aba0dd82
Siena Villancio-Wolter <90354043+kvillancio@users.noreply.github.com>
2023-11-10 11:52:18 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -3,14 +3,14 @@
 """
 Created on Tue Oct 31 17:01:27 2023
 
-@author: Karen
+@author: 
 """
 
 import cv2
 import numpy as np
 import mapper
-image=cv2.imread("scanner_test.png")   #read in the image
-image=cv2.resize(image,(1300,800)) #resizing because opencv does not work well with bigger images
+image=cv2.imread("15mmol_1.jpg")   #read in the image
+#image=cv2.resize(image,(1300,800)) #resizing because opencv does not work well with bigger images
 orig=image.copy()
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
@@ -33,14 +33,12 @@ for c in contours:
         break
 approx=mapper.mapp(target) #find endpoints of the sheet
 
-pts=np.float32([[0,0],[800,0],[800,1000],[0,1000]])  #map to 800*800 target window
+pts=np.float32([[0,0],[1000,0],[1000,800],[0,800]])  #map to 800*800 target window
 
 op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
-dst=cv2.warpPerspective(orig,op,(800,1000))
+dst=cv2.warpPerspective(orig,op,(1000,800))
 
+cv2.imwrite("15_scan.jpg",dst)
 
-cv2.imshow("Scanned",dst)
-cv2.imwrite("scanned_test.png",dst)
-# press q or Esc to close
 cv2.waitKey(0)
 cv2.destroyAllWindows()
