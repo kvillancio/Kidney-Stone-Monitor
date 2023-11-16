a3c9deff7c406994393a9e4780da7083657df306
kathrynkalouf <101954586+kathrynkalouf@users.noreply.github.com>
2023-11-16 15:30:04 -0500

    Update color_compare.py

--- color_compare.py
+++ color_compare.py
@@ -6,6 +6,13 @@ COLORS = (
     (247,235,235),
     (241,215,224),
     (229,191,214),
+    (248,178,28),
+    (249,192,89),
+    (249,210,93),
+    (229,197,1),
+    (194,187,8),
+    (171,179,16),
+    (95,250,219)
 )
 
 ##this code came from stack overflow by mVCHr and edited by Adam Jenca
@@ -17,6 +24,8 @@ def closest_color(rgb):
         color_diff = math.sqrt((r - cr)**2 + (g - cg)**2 + (b - cb)**2)
         color_diffs.append((color_diff, color))
     return min(color_diffs)[1]
+
+
 
 
 
