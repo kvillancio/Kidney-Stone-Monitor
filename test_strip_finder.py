e902c45535f3134726130e6ec2cbe696edfedf03
Siena Villancio-Wolter <90354043+kvillancio@users.noreply.github.com>
2024-03-05 14:23:55 -0500

    New urine test strip locator with lines

---
+++ test_strip_finder.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+def ph_strip_finder(filename):
+    # Load image
+    img = cv2.imread(filename)
+    
+    # Get image dimensions
+    h, w, _ = img.shape
+    
+    # Status variable for loop
+    status = 0
+    
+    # Loop to detect dark line next to pH square
+    for x in range(500,w):
+        for y in range(h):
+            col = img[y, x, :]
+            if col[0] <= 60:  # x > 500 is for the dust particle
+                status = 1
+                break
+        if status == 1:
+            break
+    
+    # Initialize vectors
+    r = []
+    g = []
+    b = []
+    
+    # Loop to collect RGB values to the right of the detected point
+    for j in range(2):
+        for i in range(100, 121):
+            col_up = img[y + j, x + i, :]
+            r.append(col_up[0])
+            g.append(col_up[1])
+            b.append(col_up[2])
+            col_down = img[y - j, x + i, :]
+            r.append(col_down[0])
+            g.append(col_down[1])
+            b.append(col_down[2])
+    
+    # Calculate average RGB values
+    avg_ph_col = [np.mean(b), np.mean(g), np.mean(r)]
+    
+    print("Average pH RGB Values:", avg_ph_col)
+    
+    return avg_ph_col
+
+def calc_strip_finder(filename):
+    # Load image
+    img = cv2.imread(filename)
+    
+    # Get image dimensions
+    h, w, _ = img.shape
+    
+    # Status variable for loop
+    status = 0
+    
+    # Loop to detect dark line
+    for x in range(500,w):
+        for y in range(h):
+            col = img[y, x, :]
+            if col[0] <= 60:  # Check if the red component is dark
+                for y2 in range(y+5,h):
+                    col = img[y2, x+5, :]
+                    if col[0] <= 60:
+                        status = 1;
+                        break
+        if status == 1:
+            break
+    
+    # Initialize vectors
+    r = []
+    g = []
+    b = []
+    
+    # Loop to collect RGB values to the right of the detected point
+    for j in range(2):
+        for i in range(100, 121):
+            col_up = img[y2 + j, x + i, :]
+            r.append(col_up[0])
+            g.append(col_up[1])
+            b.append(col_up[2])
+            col_down = img[y2 - j, x + i, :]
+            r.append(col_down[0])
+            g.append(col_down[1])
+            b.append(col_down[2])
+            
+    
+    # Calculate average RGB values
+    avg_calc_col = [np.mean(b), np.mean(g), np.mean(r)]
+    
+    print("Average Calcium RGB Values:", avg_calc_col)
+    
+    return avg_calc_col
+
+filename = 'strip_line_test.jpg'
+ph_rgb = ph_strip_finder(filename)
+calc_rgb = calc_strip_finder(filename)
