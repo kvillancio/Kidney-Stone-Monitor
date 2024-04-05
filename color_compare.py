18b4fbf65aae79db274d8267ae01d90319d189df
Siena Villancio-Wolter <90354043+kvillancio@users.noreply.github.com>
2024-04-05 18:45:39 -0400

    UPDATED COLOR COMPARE

--- color_compare.py
+++ color_compare.py
@@ -15,23 +15,23 @@ import math
 # )
 
 ##this code came from stack overflow by mVCHr and edited by Adam Jenca
-def closest_color(cal_strip, pH_strip, cal_chart):
+def closest_color(cal_strip, pH_strip, chart_ph,chart_ca):
     r_cal, g_cal, b_cal = cal_strip
     r_pH, g_pH, b_pH = pH_strip
     color_diffs_calcium = []
-    for color in cal_chart:
+    for color in chart_ca:
         cr, cg, cb = color
         color_diff = math.sqrt((r_cal - cr)**2 + (g_cal - cg)**2 + (b_cal - cb)**2)
         color_diffs_calcium.append((color_diff, color))
     
     color_diffs_pH = []
-    for color in cal_chart:
+    for color in chart_ph:
         ce, cf, cj = color
         color_diff = math.sqrt((r_pH - cr)**2 + (g_pH - cg)**2 + (b_pH - cb)**2)
         color_diffs_pH.append((color_diff, color))
-        closest_calcium = min(color_diffs_calcium)[1]
-        closest_pH = min(color_diffs_pH)[1]
-        return color_diffs_calcium.index(closest_calcium), color_diffs_pH.index(closest_pH)
+    closest_calcium = min(color_diffs_calcium)[1]
+    closest_pH = min(color_diffs_pH)[1]
+    return color_diffs_calcium.index(closest_calcium), color_diffs_pH.index(closest_pH)
 
 
 
